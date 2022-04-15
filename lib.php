04af1da07431829484dcc80c0d2e93125de5268f
Kaleb Heitzman <kaleb@jamfire.io>
2022-04-15 13:34:41 -0400

    updated translate page with course context

--- lib.php
+++ lib.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Add Translate Link to Edit Settings
+ *
+ * @param object $navigation
+ * @param object $course
+ * @param object $coursecontext
+ * @return void
+ */
 function filter_translatable_extend_navigation_course($navigation, $course, $coursecontext) {
     global $COURSE;
 
@@ -8,24 +16,3 @@ function filter_translatable_extend_navigation_course($navigation, $course, $cou
     $navigation->add_node($translatecontent);
 }
 
-// function filter_translatable_before_footer() {
-//     global $PAGE, $SESSION, $CFG;
-
-//     if (isset($SESSION->filter_translatable->strings)) {
-//         $strings = $SESSION->filter_translatable->strings;
-//     } else {
-//         $strings = [];
-//     }
-
-//     if (has_capability('filter/translatable:edittranslations', \context_system::instance())) {
-//         if (get_config('filter_translatable', 'showstringsinfooter') !=  0) {
-//             foreach ($strings as $key => $value) {
-//                 $id = $key;
-//                 echo  $value .' <a target="_blank" data-action="translation-edit" data-recordid="'.$id.'" href="'.$CFG->wwwroot.'/admin/tool/translationmanager/edit.php?id='.$id.'">
-//                 <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a><br/>';
-//             }
-//         }
-//         $PAGE->requires->js_call_amd('tool_translationmanager/translationmanager', 'init');
-//     }
-//     unset($SESSION->filter_translatable->strings);
-// }
